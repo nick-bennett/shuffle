241663ed510938295faa17e628dc4cdf56187b81
Nick Bennett <nick@nickbenn.com>
2021-06-15 09:15:45 -0600

    Modified build to include docs/api.

--- src/main/java/edu/cnm/deepdive/Shuffler.java
+++ src/main/java/edu/cnm/deepdive/Shuffler.java
@@ -2,10 +2,21 @@ package edu.cnm.deepdive;
 
 import java.util.Random;
 
+/**
+ * Implements <a href="https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle">Fisher-Yates</a>
+ * shuffle for arrays of all primitive data types, as well as of {@link Object}. Shuffling is done
+ * in-place&mdash;that is, the methods don't return copies of the original arrays, but instead
+ * modify the contents of those arrays directly.
+ */
 public class Shuffler {
 
   private final Random rng;
 
+  /**
+   * Initializes this instance, using the provided source of randomness.
+   *
+   * @param rng Source of randomness.
+   */
   public Shuffler(Random rng) {
     this.rng = rng;
   }
@@ -14,6 +25,24 @@ public class Shuffler {
     for (int targetIndex = data.length - 1; targetIndex > 0; targetIndex--) {
       int sourceIndex = rng.nextInt(targetIndex + 1);
       int temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
+  public void shuffle(byte[] data) {
+    for (int targetIndex = data.length - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      byte temp = data[targetIndex];
+      data[targetIndex] = data[sourceIndex];
+      data[sourceIndex] = temp;
+    }
+  }
+
+  public void shuffle(Object[] data) {
+    for (int targetIndex = data.length - 1; targetIndex > 0; targetIndex--) {
+      int sourceIndex = rng.nextInt(targetIndex + 1);
+      Object temp = data[targetIndex];
       data[targetIndex] = data[sourceIndex];
       data[sourceIndex] = temp;
     }
